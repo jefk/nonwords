db3490e9f45d0b36eb20ab01086cf2878a3fe074
jefk <speedracer111@gmail.com>
2013-06-21 21:59:33 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nonword generator
+
+this is a work in progress
---
+++ word_model.py
@@ -0,0 +1,42 @@
+
+import sys
+import itertools
+import collections
+import nltk
+
+class WordModel:
+
+    def __init__(self, stream):
+        self.input = stream 
+        self.text = self._set_text()
+        self.gram_sizes = [1, 2, 3]
+        self.model = collections.Counter()
+        self._generate_model()
+
+    def all_words(self):
+        return nltk.tokenize.wordpunct_tokenize(self.text)
+
+    def filtered_words(self):
+        return ( word for word in self.all_words() if len(word) > 3 )
+
+    def _generate_model(self):
+        for word in self.filtered_words():
+            chars = list(word)
+            for gram in self._all_grams(chars):
+                self.model[gram] += 1
+
+    def _all_grams(self, ls):
+        flattenable = ( self._golden_grams(ls, size) for size in self.gram_sizes )
+        return itertools.chain.from_iterable(flattenable)
+
+    def _golden_grams(self, ls, n):
+        decorated_ls = (n-1) * ['<begin>'] + ls + (n-1) * ['<end>']
+        indexes = xrange(len(ls) + n - 1)
+        return ( tuple(decorated_ls[i:i+n]) for i in indexes )
+
+    def _set_text(self):
+        lines = [ line.strip() for line in self.input ]
+        return ' '.join(lines)
+
+if __name__ == '__main__':
+    model = WordModel(sys.stdin) 
